9d595c61447662d2c64290a1add3e01f9613a409
developer <aprajapati@bestpeers.com>
2024-07-05 19:01:00 +0530

    sql.txt file

---
+++ data_types.py
@@ -0,0 +1,68 @@
+# Python Data Types
+'''
+Numeric (intger , complex , float)
+Sequence Type (list, tuple, strings)
+Boolean
+Set
+Dictionary
+Binary Types( memoryview, bytearray, bytes)
+'''
+
+# Numeric
+
+'''
+Integers =
+    This value is represented by int class. It contains positive or negative whole numbers (without fractions or decimals). In Python, there is no limit to how long an integer value can be.
+Float =
+    This value is represented by the float class. It is a real number with a floating-point representation. It is specified by a decimal point. Optionally, the character e or E followed by a positive or negative integer may be appended to specify scientific notation.
+Complex Numbers =
+    A complex number is represented by a complex class. It is specified as (real part) + (imaginary part)j. For example = 2+3j
+
+# note: type() function is use define the data type
+x = 10
+print(type(x))
+'''
+
+
+a = ("Geeks", "for", "Geeks")
+b = ["Geeks", "for", "Geeks"]
+c = {"Geeks": 1, "for":2, "Geeks":3}
+d = "Hello World"
+e = 10.23
+f = 11.22
+ 
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+
+
+# Sequence Type
+
+'''
+The sequence Data Type in Python is the ordered collection of similar or different Python data types. Sequences allow storing of multiple values in an organized and efficient fashion. There are several sequence data types of Python:
+
+Python String
+Python List
+Python Tuple
+
+
+String Data Type
+
+    Strings in Python are arrays of bytes representing Unicode characters. A string is a collection of one or more characters put in a single quote, double-quote, or triple-quote. In Python, there is no character data type Python, a character is a string of length one. It is represented by str class. 
+
+    Creating String
+    Strings in Python can be created using single quotes, double quotes, or even triple quotes.
+
+List Data Type
+    Lists are just like arrays, declared in other languages which is an ordered collection of data. It is very flexible as the items in a list do not need to be of the same type.  
+
+    Creating a List in Python
+    Lists in Python can be created by just placing the sequence inside the square brackets[]. 
+
+M    
+    
+'''
+
